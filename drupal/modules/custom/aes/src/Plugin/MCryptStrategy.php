90219d2269b2b1a0bdd98696d00a3bea7f8c37f6
Dennis Povshedny <basturma@gmail.com>
2014-11-02 13:35:19 +0200

    Recovering bad commit

---
+++ drupal/modules/custom/aes/src/Plugin/MCryptStrategy.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennis
+ * Date: 2/11/14
+ * Time: 12:52 PM
+ */
+
+namespace Drupal\aes\Plugin;
+
+
+class MCryptStrategy implements ICrypt{
+
+  public function encrypt($string){
+
+  }
+
+  public function decrypt($string){
+
+  }
+} 
---
+++ drupal/modules/custom/aes/src/Plugin/PhpSecLibStrategy.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dennis
+ * Date: 2/11/14
+ * Time: 12:57 PM
+ */
+
+namespace Drupal\aes\Plugin;
+
+class PhpSecLibStrategy {
+
+  public function encrypt($string){
+
+  }
+
+  public function decrypt($string){
+
+  }
+} 
